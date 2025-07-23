9785de426d7adf2b2fab65a5b3c13c6916e6e9bc
jabbiblanias <johnaprilbiblanias87@gmail.com>
2025-07-23 23:15:53 +0800

    loan updated but not yet tested

--- loans/views.py
+++ loans/views.py
@@ -1,35 +1,77 @@
 from django.shortcuts import render, redirect
-from models import Loans, Member
-from datetime import date
+from .models import LoanApplication, Member
 from django.contrib.auth.decorators import login_required
+from datetime import date
 
 @login_required
 def apply_loan(request):
     if request.method == 'POST':
         user=request.user
-        member=Member.objects.get(user_id=user)
 
         loan_type = request.POST.get('loanType')
         loan_amount = request.POST.get('loanAmount')
         interest_rate = request.POST.get('interestRate')
         loan_term = request.POST.get('loanTerm')
-        repayment_frequency = request.POST.get('loanType')
-        total_payable = request.POST.get('loanType')
-        monthly_amortization = request.POST.get('loanType')
-        penalty_rate = request.POST.get('loanType')
-        service_charge = request.POST.get('loanType')
-        net_proceeds = request.POST.get('loanType') 
+        total_payable = request.POST.get('totalPayable')
+        monthly_amortization = request.POST.get('monthlyAmortization')
+        cbu = request.POST.get('cbu')
+        insurance = request.POST.get('insurance')
+        service_charge = request.POST.get('serviceCharge')
+        net_proceeds = request.POST.get('netProceeds') 
 
-        Loans.objects.create(
-            member=member,
+        if user.groups.filter(name='Bookkeeper').exists():
+            account_number=request.POST.get('accountNumber')
+            member=Member.objects.get(account_number=account_number)
+        else:
+            member=Member.objects.get(user_id=user)
+
+        LoanApplication.objects.create(
+            member_id=member,
             loan_type=loan_type,
             loan_amount=loan_amount,
             interest_rate=interest_rate,
-            loan_term=loan_term,
-            repayment_frequency=repayment_frequency,
+            loan_term_years=loan_term,
+            loan_term_months=loan_term,
+            loan_term_days=loan_term,
             total_payable=total_payable,
             monthly_amortization=monthly_amortization,
-            penalty_rate=penalty_rate,
+            cbu=cbu,
+            insurance=insurance,
             service_charge=service_charge,
             net_proceeds=net_proceeds
         )
+def approving_loan(request):
+    if request.method == 'POST':
+        user = request.user
+        loan_application_id = request.POST.get('applicationid')
+
+        try:
+            loan_application = LoanApplication.objects.get(loan_application_id=loan_application_id)
+
+            if user.groups.filter('Bookkeeper').exists():
+                loan_application.verifier_id = user
+                loan_application.verified_date = date.today()
+                loan_application.save()
+            elif user.groups.filter('Admin').exists():
+                loan_application.approver_id = user
+                loan_application.approved_date = date.today()
+                loan_application.save()
+        except LoanApplication.DoesNotExist:
+            print("Loan application record not found.")
+        
+def loan_release(request):
+    if request.method == 'POST':
+        user = request.user
+        loan_application_id = request.POST.get('applicationid')
+
+        try:
+            loan_application = LoanApplication.objects.get(loan_application_id=loan_application_id)
+
+            if user.groups.filter('Cashier').exists():
+                loan_application.released_by_id = user
+                loan_application.released_date = date.today()
+                loan_application.save()
+        except LoanApplication.DoesNotExist:
+            print("Loan application record not found.")
+        
+        
