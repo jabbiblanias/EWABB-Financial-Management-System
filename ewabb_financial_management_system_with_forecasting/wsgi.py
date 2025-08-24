48e3c620824b699a14e84ba3ac85fc0e8ecb2794
jabbiblanias <johnaprilbiblanias87@gmail.com>
2025-08-24 12:18:34 +0800

    change back

--- ewabb_financial_management_system_with_forecasting/wsgi.py
+++ ewabb_financial_management_system_with_forecasting/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ewabb_financial_management_system_with_forecasting.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ewabb_financial_management_system_with_forecasting.settings')
 
 application = get_wsgi_application()
