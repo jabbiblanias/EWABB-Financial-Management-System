a2d22a2ec6da9b769ff0ceb2df5d8f97546a7872
jabbiblanias <johnaprilbiblanias87@gmail.com>
2025-10-09 15:33:00 +0800

    vercel

--- ewabb_financial_management_system_with_forecasting/settings.py
+++ ewabb_financial_management_system_with_forecasting/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
