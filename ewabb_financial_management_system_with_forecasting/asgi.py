415c443fe5fe9b16d1ada2212988f175ccae4d7b
jabbiblanias <johnaprilbiblanias87@gmail.com>
2025-10-09 09:49:07 +0800

    update setting

--- ewabb_financial_management_system_with_forecasting/asgi.py
+++ ewabb_financial_management_system_with_forecasting/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ewabb_financial_management_system_with_forecasting.settings')
 
-application = get_asgi_application()
+app = get_asgi_application()
