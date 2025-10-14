75dfb6ee71f623a290de65d61cb52a4277a101c2
jabbiblanias <johnaprilbiblanias87@gmail.com>
2025-10-14 21:05:50 +0800

    cron

--- loans/management/commands/update_repayment_status.py
+++ loans/management/commands/update_repayment_status.py
@@ -6,12 +6,18 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         today = timezone.localdate()
+        EXCLUDE_STATUSES = ['Paid', 'Canceled']
+
         updated_due = LoanRepaymentSchedule.objects.filter(
             due_date=today
+        ).exclude(
+            status__in=EXCLUDE_STATUSES
         ).update(status='Due')
 
         updated_overdue = LoanRepaymentSchedule.objects.filter(
             due_date__lt=today
+        ).exclude(
+            status__in=EXCLUDE_STATUSES
         ).update(status='Overdue')
 
         self.stdout.write(self.style.SUCCESS(
