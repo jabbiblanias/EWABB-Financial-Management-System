70704db5021c51b4089dbe2f4846687b3ebb831e
jabbiblanias <johnaprilbiblanias87@gmail.com>
2026-02-11 11:37:40 +0800

    revert

--- ewabb_financial_management_system_with_forecasting/settings.py
+++ ewabb_financial_management_system_with_forecasting/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
-DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
